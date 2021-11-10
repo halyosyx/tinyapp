b863e66fe726b2a6b120f437c88efddfcf4b39eb
vagrant <aisle.github@gmail.com>
2021-11-10 00:48:54 +0000

    update

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
 const { json } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -19,12 +22,38 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+
+
+// Generate a random string for shortURL
+const generateRandomString = function () {
+  const stringLength = 6;
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let shortURL = '';
+
+  for (let i = 0; i < stringLength; i++) {
+    shortURL += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+
+  return shortURL;
+
+}
+
+console.log(generateRandomString());
