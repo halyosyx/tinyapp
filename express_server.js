6f266039816ee382c1e4a2b721efcbc1e4519fbd
vagrant <aisle.github@gmail.com>
2021-11-11 05:18:44 +0000

    Implemented login and logout feature

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const { json } = require('express');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -13,16 +15,17 @@ const urlDatabase = {
 };
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { username: req.cookies['username'],urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies['username']};
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {username: req.cookies['username'], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
 
@@ -38,8 +41,6 @@ app.post('/urls/:shortURL', (req, res) => {
   urlDatabase[shortUrl] = req.body.longURL;
   res.redirect('/urls')
 });
-
-
 
 app.post('/urls', (req, res) => {
   //req.body.shortURL = generateRandomString();
@@ -67,7 +68,16 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[url];
   res.redirect('/urls');
 
-})
+});
+
+app.post('/login', (req, res) => {
+  const user = req.body.username;
+  
+  res.cookie('username', user);
+
+  res.redirect('/urls');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
